2938ec32fec316ba420a831741c10f3cd147d1ec
VMGit <91979333+SelfishMack@users.noreply.github.com>
2022-12-28 10:51:54 +0300

    Add files via upload

---
+++ multi-section-collectionview/MultiSectionCompositionalLayout/ListItem.swift
@@ -0,0 +1,13 @@
+//
+//  ListItem.swift
+//  MultiSectionCompositionalLayout
+//
+//  Created by Владимир on 18.05.2022.
+//
+
+import Foundation
+
+struct ListItem {
+    let title: String
+    let image: String
+}
